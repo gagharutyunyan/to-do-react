cf676eaae201bb681eb707651e8146e5d72ecf09
gagharutyunyan <gagharut@gmail.com>
2019-07-29 22:05:18 +0300

    refactoring add-list

--- src/components/add-list/add-list.js
+++ src/components/add-list/add-list.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './add-list.css';
 
-const AddList = ({ listValue, listSubmit, listAddingValue }) => {
-	return (
+const AddList = ({ listValue, listSubmit, listAddingValue }) => (
 		<form onSubmit={(e) => listSubmit(e)}>
 			<input className='add_bar' onChange={(e) => listValue(e)} value={listAddingValue} placeholder='type anything and add' autoFocus/>
 			<button className='add_btn'></button>
 		</form>
 	);
-};
 
 export default AddList;
