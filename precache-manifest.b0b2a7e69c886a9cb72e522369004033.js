fabd6664d37f16dbd4dfb8b0ea449781bdd1d3b4
gagharutyunyan <gagharut@gmail.com>
2019-07-30 18:17:28 +0300

    Updates

---
+++ precache-manifest.b0b2a7e69c886a9cb72e522369004033.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "101a91318d694b7e63c2d606ee29dc12",
+    "url": "/to-do-react/index.html"
+  },
+  {
+    "revision": "4a7ac0c28584b28ad054",
+    "url": "/to-do-react/static/css/main.f4db653c.chunk.css"
+  },
+  {
+    "revision": "187eca812d63075e5bc7",
+    "url": "/to-do-react/static/js/2.b9414670.chunk.js"
+  },
+  {
+    "revision": "4a7ac0c28584b28ad054",
+    "url": "/to-do-react/static/js/main.ef15ad52.chunk.js"
+  },
+  {
+    "revision": "656b3439b57802ac4816",
+    "url": "/to-do-react/static/js/runtime~main.c894c534.js"
+  },
+  {
+    "revision": "d5098b2c3d1417da8ccd9764612248ca",
+    "url": "/to-do-react/static/media/bgcolor.d5098b2c.png"
+  }
+]);
